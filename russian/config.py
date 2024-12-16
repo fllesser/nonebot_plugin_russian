dc5acc81c9db8262c1b41b3affaf330c2b8472be
Les Freire <64878354+fllesser@users.noreply.github.com>
2024-12-17 00:00:02 +0800

    update russian/config.py

--- russian/config.py
+++ russian/config.py
@@ -1,9 +1,8 @@
 from typing import Tuple
 from pydantic import BaseModel, Extra
-from pathlib import Path
 
 
 class Config(BaseModel, extra=Extra.ignore):
     max_bet_gold: int = 10000000
     sign_gold: Tuple[int, int] = (1, 100)
-    russian_path: Path = Path()
+    # russian_path: Path = Path()
