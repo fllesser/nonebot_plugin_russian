3357612a557df9dcf2ac869744839620552fb616
Les Freire <64878354+fllesser@users.noreply.github.com>
2024-12-17 00:11:07 +0800

    update russian/model.py

---
+++ russian/model.py
@@ -0,0 +1,26 @@
+from datetime import date
+from typing_extensions import TypedDict
+
+from nonebot_plugin_orm import Model
+from sqlalchemy.orm import Mapped, mapped_column
+
+
+class Player(Model):
+    __tablename__ = "russian_players"
+
+    gid: Mapped[int] = mapped_column(primary_key=True)
+    """Group ID"""
+    uid: Mapped[int] = mapped_column(primary_key=True)
+    """User ID"""
+    gold: Mapped[int] = mapped_column(default=0)
+    """User's Gold"""
+    make_gold: Mapped[int] = mapped_column(default=0)
+    """User's Make Gold"""
+    lose_gold: Mapped[int] = mapped_column(default=0)
+    """User's Lose Gold"""
+    win_count: Mapped[int] = mapped_column(default=0)
+    """User's Win Count"""
+    lose_count: Mapped[int] = mapped_column(default=0)
+    """User's Lose Count"""
+    last_sign: Mapped[date]
+    """Last Sign Date"""
